54a686130fc7453b3ed71a58f512c91deef2cd91
Rhama Krisner <66706928+rhama-krisner@users.noreply.github.com>
2022-04-03 00:52:25 -0300

    1002

---
+++ 1002/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        double raio = scanner.nextDouble();
+        double result = 3.14159*(raio*raio);
+        System.out.println(String.format("A=%.4f",result));
+    }
+}
